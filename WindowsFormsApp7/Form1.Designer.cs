9ceb656baf9c4f6b7fca4897175e876c12ded179
fukur <fukur@LAPTOP-7EQMGM6H>
2020-05-07 21:00:19 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp7/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace WindowsFormsApp7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rez = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // rez
+            // 
+            this.rez.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.rez.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.rez.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.rez.Location = new System.Drawing.Point(29, 37);
+            this.rez.Multiline = true;
+            this.rez.Name = "rez";
+            this.rez.ReadOnly = true;
+            this.rez.Size = new System.Drawing.Size(414, 320);
+            this.rez.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(530, 450);
+            this.Controls.Add(this.rez);
+            this.Name = "Form1";
+            this.Text = "pan";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox rez;
+    }
+}
+
